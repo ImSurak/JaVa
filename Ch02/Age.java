5a5f537cb96ad27772553c1ca340c29b2bea8620
ImSurak <97578183+ImSurak@users.noreply.github.com>
2022-02-08 21:07:15 +0900

    Add files via upload

---
+++ Ch02/Age.java
@@ -0,0 +1,13 @@
+package chapter2;
+
+public class Age {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int Age;
+		Age = 27;
+		System.out.println(Age);
+	
+	}
+
+}
