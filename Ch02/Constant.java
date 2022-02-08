0e4d79441d414b0d544951e06e845d50e66c4e61
ImSurak <97578183+ImSurak@users.noreply.github.com>
2022-02-08 21:08:13 +0900

    Add files via upload

---
+++ Ch02/Constant.java
@@ -0,0 +1,21 @@
+package chapter2;
+
+public class Constant {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final int MAX_NUM = 100;
+		final int MIN_NUM;
+		
+		MIN_NUM = 0;
+		
+		System.out.println(MAX_NUM);
+		System.out.println(MIN_NUM);
+			
+			int MY_AGE = 27;
+			System.out.println(MY_AGE);
+		
+		
+	}
+
+}
---
+++ Ch02/IntegerVariable.java
@@ -0,0 +1,13 @@
+package chapter2;
+
+public class IntegerVariable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		short sVal = 10;
+		byte bVal = 20;
+		System.out.println(sVal+bVal);
+		
+	}
+
+}
