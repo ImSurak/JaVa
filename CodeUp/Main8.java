541246b1329e0f4a4c0c6daa094b99590617a51b
ImSurak <97578183+ImSurak@users.noreply.github.com>
2022-03-03 17:47:47 +0900

    요약
    변수를 scanner로 입력하기 위해서는 next()라는 문구를 쓴다. 예를 들어 int형이면 nextInt(); float형이면 nextFloat(); 이렇게 쓴다. 하지만 문자열. Char 형에는 next().charAt(0);라는 문구를 쓴다.

---
+++ CodeUp/Main8.java
@@ -0,0 +1,12 @@
+package Code;
+import java.util.Scanner;
+public class Main8 {
+
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    char i;
+	    i = sc.next().charAt(0);
+	    
+	    System.out.println(i);
+	  }
+	}
