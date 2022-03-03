5487a769e48594e994145ec39972d10ffb3e237b
ImSurak <97578183+ImSurak@users.noreply.github.com>
2022-03-03 17:55:17 +0900

    Add files via upload
    입력문과 여러 조건문을 이용한 코드

---
+++ CodeUp/Main15.java
@@ -0,0 +1,28 @@
+package Code;
+import java.util.Scanner;
+import java.util.Scanner;
+
+public class Main15 {
+
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    int a;
+	    int b;
+	    int c;
+
+	    a = sc.nextInt();
+	    b = sc.nextInt();
+	    c = sc.nextInt();
+
+	    if(a%2==0){
+	      System.out.println(a);
+	    }
+	    if(b%2==0){
+	      System.out.println(b);
+	    }
+	    if(c%2==0){
+	      System.out.println(c);
+	    }
+	    
+	  }
+	}
