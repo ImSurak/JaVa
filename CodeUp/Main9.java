6a57136247e25d25d2c7a56936d7986bd6923f63
ImSurak <97578183+ImSurak@users.noreply.github.com>
2022-03-03 17:49:45 +0900

    요약
    나는 지금까지 자바 출력하는 명령으로 System.out.println(); 이라는 문구를 썼지만 System.out.printf("%f", i); 를 썼다. 이것은 float 변수를 출력하기 위한 출력문이다.

---
+++ CodeUp/Main9.java
@@ -0,0 +1,11 @@
+package Code;
+import java.util.Scanner;
+public class Main9 {
+
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    float i;
+	    i = sc.nextFloat();
+	    System.out.printf("%f", i);
+	  }
+	}
