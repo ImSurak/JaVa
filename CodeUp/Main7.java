8c7a46bde0d4c498663573ed9f26211505190f8f
ImSurak <97578183+ImSurak@users.noreply.github.com>
2022-03-03 17:43:20 +0900

    Add files via upload

---
+++ CodeUp/Main7.java
@@ -0,0 +1,10 @@
+package Code;
+import java.util.Scanner;
+public class Main7 {
+    Scanner sc = new Scanner(System.in);
+    int i;
+    i = sc.nextInt();
+    
+    System.out.println(i);
+  }
+}
